7e9d43b7ce4c7fdf544732b4671bf7f6025f6710
Xavi Moll <xmollv@gmail.com>
2015-08-12 16:43:37 +0200

    Finished the twelfth project.

---
+++ Tutorial 12 - NSUserDefaults/Project12/Project10/Person.swift
@@ -0,0 +1,29 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Xavi Moll on 5/8/15.
+//  Copyright (c) 2015 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("name") as! String
+        image = aDecoder.decodeObjectForKey("image") as! String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: "name")
+        aCoder.encodeObject(image, forKey: "image")
+    }
+}
