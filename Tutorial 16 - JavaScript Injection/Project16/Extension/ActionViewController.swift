41deaaefcada2d0f8173009f17f37cee63da4b83
Xavi Moll <xmollv@gmail.com>
2015-08-16 16:44:57 +0200

    Finished the sixteenth project.

---
+++ Tutorial 16 - JavaScript Injection/Project16/Extension/ActionViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Xavi Moll on 9/8/15.
+//  Copyright (c) 2015 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    var pageTitle = ""
+    var pageURL = ""
+    
+    @IBOutlet weak var script: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let inputItem = extensionContext!.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first as? NSItemProvider {
+                itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { [unowned self] (dict, error) in
+                    // do stuff!
+                    let itemDictionary = dict as! NSDictionary
+                    let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+                    
+                    self.pageTitle = javaScriptValues["title"] as! String
+                    self.pageURL = javaScriptValues["URL"] as! String
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.title = self.pageTitle
+                    }
+                }
+            }
+        }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "done")
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        let item = NSExtensionItem()
+        let webDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: ["customJavaScript": script.text]]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext!.completeRequestReturningItems([item], completionHandler: nil)
+    }
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            script.contentInset = UIEdgeInsetsZero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
+
+}
---
+++ Tutorial 16 - JavaScript Injection/Project16/Project16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Xavi Moll on 9/8/15.
+//  Copyright (c) 2015 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
