da62ee95e2636da85d2386b85b773c915967c9f0
Xavi Moll <xmollv@gmail.com>
2015-08-23 16:47:01 +0200

    Finished the twenty-third project.

---
+++ Tutorial 23 - Space Race/Project23/Project23/GameScene.swift
@@ -0,0 +1,114 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Xavi Moll on 13/8/15.
+//  Copyright (c) 2015 Xavi Moll. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: NSTimer!
+    var gameOver = false
+    
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+		
+        backgroundColor = UIColor.blackColor()
+        
+        let starfieldPath = NSBundle.mainBundle().pathForResource("Starfield", ofType: "sks")!
+        starfield = NSKeyedUnarchiver.unarchiveObjectWithFile(starfieldPath) as! SKEmitterNode
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture, size: player.size)
+        player.physicsBody!.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .Left
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(0.35, target: self, selector: "createEnemy", userInfo: nil, repeats: true)
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        for node in children as! [SKNode] {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        
+        if !gameOver {
+            score += 1
+        }
+    }
+    
+    func createEnemy() {
+        possibleEnemies.shuffle()
+        
+        let sprite = SKSpriteNode(imageNamed: possibleEnemies[0])
+        sprite.position = CGPoint(x: 1200, y: RandomInt(min: 50, max: 736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        var location = touch.locationInNode(self)
+        
+        if location.y < 100 {
+            location.y = 100
+        } else if location.y > 668 {
+            location.y = 668
+        }
+        
+        player.position = location
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let explosionPath = NSBundle.mainBundle().pathForResource("explosion", ofType: "sks")!
+        let explosion = NSKeyedUnarchiver.unarchiveObjectWithFile(explosionPath) as! SKEmitterNode
+        explosion.position = player.position
+        addChild(explosion)
+        
+        player.removeFromParent()
+        
+        gameOver = true
+    }
+}
